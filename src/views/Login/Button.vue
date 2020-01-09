ec7a28281d78be3893ecfdb1804c04e3e7d4dfdd
yuuta3594 <yuuta3594@outlook.jp>
2020-01-09 11:35:46 +0900

    login

---
+++ src/views/Login/Button.vue
@@ -0,0 +1,54 @@
+<template>
+  <input :style="style" type="submit" :value="value">
+</template>
+
+<script>
+export default {
+  name: "Field",
+  props: {
+    value: {
+      default: '',
+    },
+    valid: {
+      default: null,
+    }
+  },
+  computed: {
+    color () {
+      if (this.valid == null) {
+        return '#3498db'
+      } else {
+        return this.valid ?
+          '#2ecc71' :
+          '#CC191D'
+      }
+    },
+    style () {
+      return {
+        '--color': this.color,
+        '--border': `2px solid ${this.color}`,
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  input {
+    background: none;
+    display: block;
+    margin: 20px auto;
+    text-align: center;
+    border: var(--border);
+    padding: 14px 40px;
+    outline: none;
+    color: white;
+    border-radius: 24px;
+    transition: 0.25s;
+    cursor: pointer;
+
+    &:hover {
+      background: var(--color);
+    }
+  }
+</style>
